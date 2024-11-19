40762cd8c88533d189ed554379456cb34f407861
Umair Shuja <mohammadumair142@gmail.com>
2024-11-19 18:39:51 +0500

    Deployed the flask server to PythonAnywhere

--- highlighter.js
+++ highlighter.js
@@ -88,7 +88,7 @@ document.addEventListener("mouseup", (e) => {
 
 async function extractKeywords(text) {
   try {
-    const response = await fetch('http://localhost:5000/extract_keywords', {
+    const response = await fetch('https://iumairshuja.pythonanywhere.com/extract_keywords', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
